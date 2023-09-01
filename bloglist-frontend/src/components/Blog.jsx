13ec2a3758f3228aab28b312f210908e8d11c753
adefuyeabayomi <63407534+adefuyeabayomi@users.noreply.github.com>
2023-09-01 09:49:50 +0100

    Completed 5.7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react"
 import styles from "../styles/mainStyles"
 
-const Blog = ({ blog }) => (
-  <div style={styles.blogStyle}>
-    <h3>Title : {blog.title} </h3>
-    <p><i>by </i>{ blog.author }. Find it here <a href={ blog.url }>Link</a></p>
-  </div>  
-)
+const Blog = ({ blog }) => {  
+  let [showDetails,setShowDetails] = useState(false)
+
+  const updateDetailsVisibility = () => {
+    setShowDetails(!showDetails);
+  }
+  let currentButton = <button></button>
+  currentButton = showDetails? <button onClick={updateDetailsVisibility}>Hide</button> : <button onClick={updateDetailsVisibility}>Show</button>
+  let details = <p></p>;
+  details = showDetails ? (<div>
+    <p>Author {blog.author} </p>
+    <p>Link : <a href={ blog.url }> {blog.url} </a></p>
+    <p>Likes : {blog.likes} <button>Like</button></p>
+  </div>) : <div></div>
+
+  return (
+    <div style={styles.blogStyle}>
+      <h3>Title : {blog.title} {currentButton} </h3>
+      {details}
+    </div>  
+  )
+}
 
 export default Blog
